4b5135a70a03b4fbfa110207f718a58d74bba3b9
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-04 17:55:17 +0900

    feat: register the semantic completion stage

--- src/deterministic_japanese_parser_mcp/__init__.py
+++ src/deterministic_japanese_parser_mcp/__init__.py
@@ -1,6 +1,8 @@
 from .semantic_refinement import install_semantic_refinement
+from .semantic_completion import install_semantic_completion
 
 install_semantic_refinement()
+install_semantic_completion()
 
 from .engine import ParserEngine
 from .low_latency_client import LowLatencyClientSession
