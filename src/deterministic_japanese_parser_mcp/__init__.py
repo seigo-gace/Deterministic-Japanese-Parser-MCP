25e22d7b6352022f65029be17a362a256e1b4318
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-04 18:15:29 +0900

    refactor: register contextual semantic refinement

--- src/deterministic_japanese_parser_mcp/__init__.py
+++ src/deterministic_japanese_parser_mcp/__init__.py
@@ -1,10 +1,12 @@
 from .semantic_refinement import install_semantic_refinement
 from .semantic_completion import install_semantic_completion
-from .semantic_holdout_refinement import install_semantic_holdout_refinement
+from .semantic_contextual_refinement import (
+    install_semantic_holdout_refinement as install_semantic_contextual_refinement,
+)
 
 install_semantic_refinement()
 install_semantic_completion()
-install_semantic_holdout_refinement()
+install_semantic_contextual_refinement()
 
 from .engine import ParserEngine
 from .low_latency_client import LowLatencyClientSession
