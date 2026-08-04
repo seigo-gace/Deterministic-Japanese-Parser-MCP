f98d383b4d651cc2ac716aa1facbf49c448312fe
Seigo.k <216270240+seigo-gace@users.noreply.github.com>
2026-08-04 18:06:53 +0900

    feat: register independent holdout refinements

--- src/deterministic_japanese_parser_mcp/__init__.py
+++ src/deterministic_japanese_parser_mcp/__init__.py
@@ -1,8 +1,10 @@
 from .semantic_refinement import install_semantic_refinement
 from .semantic_completion import install_semantic_completion
+from .semantic_holdout_refinement import install_semantic_holdout_refinement
 
 install_semantic_refinement()
 install_semantic_completion()
+install_semantic_holdout_refinement()
 
 from .engine import ParserEngine
 from .low_latency_client import LowLatencyClientSession
